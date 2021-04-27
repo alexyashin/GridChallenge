81fa49597c857a3230294fffd7a018a16e41f444
Alexey Yashin <ensiferumorl@gmail.com>
2021-04-27 14:19:31 +0300

    Initial Commit

---
+++ GridChallenge/GridChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridChallengeApp.swift
+//  GridChallenge
+//
+//  Created by Alexey Yashin on 27.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct GridChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
