8319584a6634b13fc758ace3412ab57ee1a1df07
Alexey Yashin <ayashin@croc.ru>
2021-04-27 14:51:21 +0300

    Show test images on grid. Change grid size on magnification gesture

--- GridChallenge/ContentView.swift
+++ GridChallenge/ContentView.swift
@@ -7,10 +7,58 @@
 
 import SwiftUI
 
+struct Photo: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+let samplePhotos = (1...20).map { Photo(name: "coffee-\($0)") }
+
 struct ContentView: View {
+    @State var gridLayout: [GridItem] = Array(repeating: GridItem(), count: 3)
+    @State var prevAmount: CGFloat = 1
+    
+    var magnification: some Gesture {
+        MagnificationGesture()
+            .onChanged { changeAmount in
+                let roundedAmount = (changeAmount * 10).rounded() / 10
+                if roundedAmount > prevAmount {
+                    self.prevAmount = roundedAmount
+                    changeGrid(toBigger: true)
+                } else if roundedAmount < prevAmount {
+                    self.prevAmount = roundedAmount
+                    changeGrid(toBigger: false)
+                }
+            }
+            .onEnded { endAmount in
+                self.prevAmount = 1
+            }
+    }
+    
+    func changeGrid(toBigger: Bool) {
+        let colCount = self.gridLayout.count
+        if toBigger && colCount < 5 {
+            self.gridLayout = Array(repeating: GridItem(), count: colCount + 1)
+        } else if !toBigger && colCount > 1 {
+            self.gridLayout = Array(repeating: GridItem(), count: colCount - 1)
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) { ForEach(samplePhotos.indices) { index in
+                Image(samplePhotos[index].name)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .frame(height: gridLayout.count == 1 ? 200 : 100)
+                    .cornerRadius(10)
+            }
+            .animation(.interactiveSpring())
+            }
+        }
+        .padding(.all, 10)
+        .gesture(magnification)
     }
 }
 
